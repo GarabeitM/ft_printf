c15c72c81e986c5aa70ef3f64fdb3bc16277a38a
Mary Garabeit <114006383+GarabeitM@users.noreply.github.com>
2023-07-13 11:24:34 +0200

    version 1

---
+++ ft_printf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgarabei <mgarabei@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 18:11:58 by mgarabei          #+#    #+#             */
+/*   Updated: 2023/07/13 11:23:54 by mgarabei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdlib.h>
+
+int		ft_char(char c);
+int		ft_str(char *args);
+int		ft_nbr(int number);
+int		ft_unsigned_int(unsigned int u);
+
+int		ft_printf(const char *string, ...);
+
+#endif
